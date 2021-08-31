90bd65f3945ec0de7a180d89fe0d28ac5df59978
sritun007 <62866409+sritun007@users.noreply.github.com>
2021-08-31 23:31:04 +0530

    Update CustomerNotFoundException.java

--- src/main/java/com/customer/exception/CustomerNotFoundException.java
+++ src/main/java/com/customer/exception/CustomerNotFoundException.java
@@ -3,6 +3,6 @@ package com.customer.exception;
 public class CustomerNotFoundException extends Exception {
 	private Integer id;
 	public CustomerNotFoundException(Integer id) {
-	        super(String.format("Customer is not found with id : '%s'", id));
+	        super(String.format("Customer is not found whose id is : '%s'", id));
 	        }
 	}
